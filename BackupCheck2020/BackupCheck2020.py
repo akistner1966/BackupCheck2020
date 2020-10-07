4a23281371ca8fc1072487c511959d7fd4cdc4bd
Andreas Kistner <akistner@t-online.de>
2020-10-07 15:35:44 +0200

    Klasse zum Vergleich der Verzeichnisse implementiert

--- BackupCheck2020/BackupCheck2020.py
+++ BackupCheck2020/BackupCheck2020.py
@@ -2,11 +2,31 @@
 #@author: comet
 
 import tkinter as tk
+from tkinter import filedialog
+
+class backupdiff(object):
+    def __init__(self, pf1, pf2):
+        pass
+
+    def gleich(self):
+        pass
+
+    def nuralt(self):
+        pass
+
+    def nurneu(self):
+        pass
 
 def bu_check():
-    pass
+    pfadneu = filedialog.askdirectory()
+    pfadalt = filedialog.askdirectory()
+    bdiff = backupdiff(pfad1, pfad2)
+    lgleich = bdiff.gleich() #Dateien in beiden Pfaden vorhanden
+    lnuralt = bdiff.nuralt() #Dateien nur im Altverzeichnis
+    lnurneu = bdiff.nurneu() #Dateien nur im Neuverzeichnis
 
 if __name__== "__main__":
+    version = '1.00'
     root = tk.Tk()
     menubar = tk.Menu(root)
     filemenu = tk.Menu(menubar, tearoff=0)
